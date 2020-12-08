ee675d23a33048f8f7618b9a308d14463beecc72
Danny <42831259+Dani340@users.noreply.github.com>
2020-12-08 12:39:54 +0200

    Add files via upload

--- 7.12.1.c
+++ 7.12.1.c
@@ -1,20 +1,40 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
-    int n,i,x=0;
-    printf("Enter the number to check if it's prime:");
+    int n,i,x,flag=1;
+    printf("Enter the number to check if it's prime: \n");
     scanf("%d",&n);
-    x=n/2;
-    for(i=2;i<=x;i++)
-    {
-        if(n%i==0)
-        {
-            printf("Number is not prime");
+    x=sqrt(n);
+    for(i=2; i <= x; i++) {
+
+        if(n%i==0) {
+            flag = 0;
             break;
         }
-        else
-        {
-            printf("Number is prime");
-            return 0;
-        }
     }
+
+
+
+    if(n==1)
+    flag=2;
+
+    else if(n==2)
+    flag=1;
+
+    else if(n<1)
+    flag=0;
+
+    if (flag == 1) {
+        printf("%d is prime", n);
+    }
+    else if (flag == 2) {
+        printf("%d is neither prime, nor composite", n);
+    }
+    else {
+        printf("%d is not prime", n);
+    }
+
+
+    return 0;
+
 }
