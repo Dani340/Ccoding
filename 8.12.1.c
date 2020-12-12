029dd443ed420509fa3d8f2390659a6895227799
Danny <42831259+Dani340@users.noreply.github.com>
2020-12-12 22:12:56 +0200

    Add files via upload

--- 8.12.1.c
+++ 8.12.1.c
@@ -1,76 +1,55 @@
 #include <stdio.h>
 #include <math.h>
 int main() {
-    int x, y, z, N, n, primeNr=0, i, j, k, p, sum=0, flag=1, flag1=1;
+    int x, N, i, j, sum = 0, primeNr = 0, isPrime;
 
     printf("Enter a number to determine the greatest prime number smaller than x: \n");
     scanf("%d", &x);
     printf("Enter N: \n");
     scanf("%d", &N);
 
-    for(i=x; i > 0; i--) {
-        n = i;
+    // pas 1: nr prim mai mic decat x
+    if(x<=1) {
+        printf("There is no number smaller than x that is prime \n");
+    } else {
+        for(i = x; i > 1; i--) {
+            isPrime = 1;
 
-        for(j=2; j <= sqrt(n); j++) {
+            for(j = 2; j <= sqrt(i); j++) {
+                if(i % j == 0) {
+                    isPrime = 0;
+                    break;
+                }
+            }
 
-            if(n%j==0) {
-                flag = 0;
+            if (isPrime == 1) {
+                printf("The result is %d \n", i);
                 break;
             }
         }
-
-        if(n==1)
-        flag=2;
-
-        else if(n==2)
-        flag=1;
-
-        else if(n<1)
-        flag=0;
-
-
-        if (flag == 1) {
-            p = n;
-            printf("The result is %d \n", p);
-            break;
-        }
-
-        else if (flag == 2) {
-            printf("There is no number smaller than x that is prime");
-        }
-
-
     }
 
-    z = x + 1;
-
+    // pas 2: suma primelor N nr prime mai mari decat x
+    x ++;
     while (primeNr < N) {
-        y=sqrt(z);
-        for(k=2; k <= y; k++) {
+        isPrime = 1;
 
-            if(z%k==0) {
-                flag1 = 0;
-                break;
+        if(x > 1) {
+            for(j = 2; j <= sqrt(x); j++) {
+                if(x % j == 0) {
+                    isPrime = 0;
+                    break;
+                }
+            }
+
+            if (isPrime == 1) {
+                primeNr ++;
+                sum += x;
             }
         }
 
-        if(z==1)
-        flag1=2;
-
-        else if(z==2)
-        flag1=1;
-
-        else if(z<1)
-        flag1=0;
-
-        if (flag1 == 1) {
-            sum += z;
-            primeNr++;
-            z++;
-
-        }
-
+        x ++;
     }
-    printf("The sum is %d ", sum);
 
+    printf("The sum is %d ", sum);
 }
