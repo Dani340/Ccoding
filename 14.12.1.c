5b74319d486cf5ad594bc4189994c100f531c0e9
Danny <42831259+Dani340@users.noreply.github.com>
2020-12-14 20:19:03 +0200

    Add files via upload

---
+++ 14.12.1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+
+    int x, N;
+    printf("Enter the number: \n");
+    scanf("%d",&N);
+
+    x = sqrt(N);
+    x = x/1;
+
+    printf("The squared number closest to N is %d" ,x);
+
+    return 0;
+
+    }
