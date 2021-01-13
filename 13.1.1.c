c224071c1e771abb48b6325abf447c61bcf327dd
Danny <42831259+Dani340@users.noreply.github.com>
2021-01-13 16:54:55 +0200

    Add files via upload

---
+++ 13.1.1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int n, k, i, p, sum=0;
+
+    printf("Enter the initial length of the nose: \n");
+    scanf("%d", &n);
+
+    printf("Enter cm per work day: \n");
+    scanf("%d", &p);
+
+    printf("Enter days of measurement: \n");
+    scanf("%d", &k);
+
+    if(k % 7 != 6) {
+        sum += n + (k / 7) * (5 * p - 2) + (k % 7) * p;
+    }
+
+    else if(k % 7 == 6) {
+        sum += n + (k / 7) * (5 * p - 2) + (5 * p - 1);
+    }
+
+    printf("His nose is now %d cm long", sum);
+
+    return 0;
+}
