59bac4b568e258c6e1f3b977f9249ba86f700007
Danny <42831259+Dani340@users.noreply.github.com>
2021-01-15 15:27:46 +0200

    Add files via upload

---
+++ 15.1.2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int A, B;
+
+    printf("Enter A: \n");
+    scanf("%d", &A);
+
+    printf("Enter B: \n");
+    scanf("%d", &B);
+
+    printf("A is %d and B is %d", B, A);
+
+    return 0;
+}
