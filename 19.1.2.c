e7fcd9ed62c39393817b20866be285ec82cecf53
Danny <42831259+Dani340@users.noreply.github.com>
2021-01-20 12:29:28 +0200

    Add files via upload

--- 19.1.2.c
+++ 19.1.2.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <string.h>
 int main()
 {
-    char c, result;
+    char string[100];
 
-    printf("Enter a word or a sentence: ");
-    scanf("%d", &c);
+    printf("Enter a word or a sentence to convert to lower case: ");
+    gets(string);
 
-    result = tolower(c);
-    printf("tolower(%c) = %c\n", c, result);
+    printf("The word or sentence lower cased is: %s", strlwr(string));
 
-    return 0;
-
+    return  0;
 }
