49b20d6d8fb99aa7b5c61fa6c65dbe994df8f613
Danny <42831259+Dani340@users.noreply.github.com>
2021-01-21 09:04:35 +0200

    Add files via upload

--- 20.1.2.c
+++ 20.1.2.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
 
-int main()
-{
-    int number1[100], number2[100], size1=0, size2=0, x, y, i, j, count=0;
+int main() {
+
+    int  x, y, copyY, count=0, digitX, digitY;
 
     printf("Enter the first number:\n");
-    scanf("%d",&number1[100]);
+    scanf("%d",&x);
 
     printf("Enter the second number:\n");
-    scanf("%d",&number2[100]);
+    scanf("%d",&y);
 
-    x = number1[100];
-    y = number2[100];
-
-    while(x != 0) {
+    while (x != 0) {
+        copyY = y;
+        digitX = x % 10;
         x = x / 10;
-        size1++;
-    }
 
-    while(y != 0) {
-        y = y / 10;
-        size2++;
-    }
+        while (copyY != 0) {
+            digitY = copyY % 10;
+            copyY = copyY / 10;
 
-    for(i = 0; i < size1; i++) {
-        for(j = 0; j < size2; j++) {
-            if(number1[i] == number2[j]) {
+            if (digitX == digitY) {
                 count++;
             }
         }
