e91aa194bb963bedf2200c400773a9dc3dad5275
Danny <42831259+Dani340@users.noreply.github.com>
2021-02-02 16:57:42 +0200

    Add files via upload

---
+++ 1.2.1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    char string1[100];
+    char X;
+    char Y;
+    int  i;
+
+    printf("Enter a word or a sentence: ");
+    gets(string1);
+
+    printf("Enter a letter: ");
+    scanf(" %c", &X);
+
+    printf("Enter another letter: ");
+    scanf(" %c", &Y);
+
+
+    for(i = strlen(string1); i > 0; i--)
+    {
+        if(string1[i] == X) {
+            string1[i] = Y;
+            break;
+        }
+    }
+
+    printf("%s", string1);
+
+
+    return 0;
+}
