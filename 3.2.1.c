26b775d0989de4e8a0b7b0847d5361bada498b48
Danny <42831259+Dani340@users.noreply.github.com>
2021-02-03 11:56:46 +0200

    Add files via upload

---
+++ 3.2.1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char string1[100];
+    int  i, count=1;
+
+    printf("Enter a word or a sentence: ");
+    gets(string1);
+
+    for(i = 0; i < strlen(string1); i++)
+    {
+        if(isspace(string1[i])) {
+            count++;
+        }
+    }
+    printf("There are %d words in the string", count);
+}
