cc4aed0a1d70fbb39c65f398c6488610bdc9ac55
Danny <42831259+Dani340@users.noreply.github.com>
2021-02-10 10:24:01 +0200

    Add files via upload

--- 17.12.2.c
+++ 17.12.2.c
@@ -1,24 +1,6 @@
 #include<stdio.h>
 #include<math.h>
-
-
-int isPerfectSquare(int rest) {
-    int isPS = 0;
-    float x;
-
-    x = sqrt(rest);
-    if(x == (int)x) {
-        isPS = 1;
-    }
-
-    if (isPS == 1) {
-        return 1;
-    } else {
-        return -1;
-    }
-}
-
-
+#include "isPerfectSquare.h"
 
 int main(){
 
