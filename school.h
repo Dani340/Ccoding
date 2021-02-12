3d368828040de51fa3f20b7702a080ea3bd410fe
Danny <42831259+Dani340@users.noreply.github.com>
2021-02-12 08:09:13 +0200

    Add files via upload

--- school.h
+++ school.h
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdio.h>
 
 int Grades(int mark) {
     if(mark >= 5) {
@@ -7,4 +8,17 @@ int Grades(int mark) {
     else {
         return false;
     }
+}
+
+float Medie(float x[], float y) {
+    float sum = 0.0;
+    int c;
+
+    for(c = 0; c < strlen(x); c++) {
+        sum += x[c];
+    }
+    sum = sum / strlen(x) * 3.0;
+    sum = (sum + y) / 4.0;
+
+    return sum;
 }
