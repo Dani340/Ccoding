9b223803ddc7ffbe6e84add0d23d588fb4732549
Danny <42831259+Dani340@users.noreply.github.com>
2021-02-16 12:07:28 +0200

    Add files via upload

---
+++ 16.2.2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+    int matrix[10][10], matrix1[10][10];
+    int i,j,r,c,x,y;
+
+    printf("Enter number of rows:");
+    scanf("%d",&r);
+    printf("Enter number of cols:");
+    scanf("%d",&c);
+    printf("Enter the x col:");
+    scanf("%d",&x);
+    printf("Enter the y col:");
+    scanf("%d",&y);
+
+
+    printf("\nEnter matrix elements :\n");
+    for(i=0;i < r;i++)
+    {
+        for(j=0;j < c;j++)
+        {
+            printf("Enter element [%d,%d] : ",i+1,j+1);
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i ++) {
+        matrix1[0][1] = matrix[i][x];
+        matrix[i][x] = matrix[i][y];
+        matrix[i][y] = matrix1[0][1];
+    }
+
+    printf("\nMatrix is :\n");
+    for(i=0;i < r;i++)
+    {
+        for(j=0;j < c;j++)
+        {
+            printf("%d\t",matrix[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
