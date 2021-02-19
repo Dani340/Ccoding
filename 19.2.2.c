20452d475b22bbbb9eb0ca460524d447bb435f47
Danny <42831259+Dani340@users.noreply.github.com>
2021-02-19 09:37:09 +0200

    Add files via upload

---
+++ 19.2.2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int matrix[10][10];
+    int i,j,r,sum=0;
+
+    printf("Enter number of rows and cols:");
+    scanf("%d",&r);
+
+    printf("\nEnter matrix elements :\n");
+    for(i=0;i < r;i++)
+    {
+        for(j=0;j < r;j++)
+        {
+            printf("Enter element [%d,%d] : ",i+1,j+1);
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < r; i++) {
+        j = i;
+        sum += matrix[i][j];
+    }
+
+    printf("Sum of numbers on 1st diag is %d ", sum);
+
+    return 0;
+}
