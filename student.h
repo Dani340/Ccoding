6ad9627ac831f088a311cea726846a92749c9dc3
Danny <42831259+Dani340@users.noreply.github.com>
2021-03-10 09:32:10 +0200

    Add files via upload

--- student.h
+++ student.h
@@ -1,21 +1,29 @@
 struct student {
    int ID[50];
-   char nume[50];
-   char prenume[50];
+   char nume[10];
+   char prenume[10];
    char sex;
    float mark;
 };
 
 void addstudent(struct student Student[], int i) {
-    struct student StudentNew;
-
-    printf("Enter the name of the %d student: ", i);
-    scanf(" %s", StudentNew.prenume);
-    printf("Enter the 2nd name of the %d student: ", i);
-    scanf(" %s", StudentNew.nume);
-    printf("Enter the sex of the %d student: ", i);
-    scanf(" %s", StudentNew.sex);
+    printf("Enter the name of the %d student: ", i+1);
+    scanf(" %s", Student[i].prenume);
+    printf("Enter the 2nd name of the %d student: ", i+1);
+    scanf(" %s", Student[i].nume);
+    printf("Enter the sex of the %d student: ", i+1);
+    scanf(" %c", &Student[i].sex);
     printf("Enter his mark: ");
-    scanf(" %f", &StudentNew.mark);
-    Student[i] = StudentNew;
+    scanf(" %f", &Student[i].mark);
+}
+
+int look4student(char prenume[], struct student Student[], int N) {
+    int count1 = 0;
+
+    for(int i = 0; i < N; i++) {
+        if(strcmp(Student[i].prenume, prenume) == 0) {
+            return -1;
+            break;
+        }
+    }
 }
