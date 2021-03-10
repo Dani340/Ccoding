5eb169893468e8df5f9243e62143c37837e8bb97
Danny <42831259+Dani340@users.noreply.github.com>
2021-03-10 09:32:00 +0200

    Add files via upload

--- 9.3.1.c
+++ 9.3.1.c
@@ -6,15 +6,27 @@ int main( ) {
 
     struct student Student1;
     struct student Student[30];
-    int i, N;
+    char prenume[10];
+    int i, N, result;
 
     printf("Enter the amount of students you want to add: ");
     scanf("%d", &N);
     printf("\n");
 
-    for(i = 1; i <= N; i++) {
+    for(i = 0; i < N; i++) {
         addstudent(Student, i);
         printf("\n");
+    }
+
+    printf("Enter the name of the student you are looking for: ");
+    scanf(" %s", &prenume);
+
+    result = look4student(prenume, Student, N);
+    if(result == -1) {
+        printf("%s is in the list of students", prenume);
+    }
+    else {
+        printf("%s is not in the list of students", prenume);
     }
 
     return 0;
