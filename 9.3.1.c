af06eeea02d972fd9a64a42f58b612975751a11c
Danny <42831259+Dani340@users.noreply.github.com>
2021-03-11 13:27:58 +0200

    Add files via upload

--- 9.3.1.c
+++ 9.3.1.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "student.h"
 
 int main( ) {
 
     struct student Student1;
     struct student Student[30];
-    char prenume[10], X[10];
-    int i, N, result;
+    char prenume[10], X[10], Y[10];
+    int i, N, result, remove, change;
+    float biggest, lowest;
 
     printf("Enter the amount of students you want to add: ");
     scanf("%d", &N);
@@ -22,23 +24,49 @@ int main( ) {
     scanf(" %s", &prenume);
 
     result = look4student(prenume, Student, N);
-    if(result == -1) {
+    if(result >= 0) {
         printf("%s is in the list of students \n", prenume);
     }
-    else {
+    else if(result < -1) {
         printf("%s is not in the list of students \n", prenume);
     }
     printf("\n");
 
-    for(i = 0; i < N; i++) {
-        printstudent(Student, i);
-        printf("\n");
+    printf("Enter the name of the student you want to remove: ");
+    scanf(" %s", &X);
+    printf("\n");
+
+    remove = removestudent(X, Student, N);
+    if(remove == 1) {
+        for (i = 0; i < N - 1; i++) {
+            printstudent(Student, i);
+            printf("\n");
+        }
+    }
+    else {
+        for (i = 0; i < N; i++) {
+            printstudent(Student, i);
+            printf("\n");
+        }
     }
 
-    printf("Enter the name of the student you want to remove: ");
-    scanf(" %c", &X);
+    printf("Enter the name of the student: ");
+    scanf(" %s", &Y);
     printf("\n");
-    removestudent(X, Student, N);
+
+    change = changemark(Y, Student, N);
+    if(change == 1) {
+        for (i = 0; i < N - 1; i++) {
+            printstudent(Student, i);
+            printf("\n");
+        }
+    }
+
+    float * BL = biggestlowestmark(Student, N);
+    biggest = BL[0];
+    lowest = BL[1];
+    printf("The lowest mark is %f \n", lowest);
+    printf("The biggest mark is %f", biggest);
 
     return 0;
 }
