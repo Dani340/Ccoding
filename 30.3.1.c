9b2251eef11134c748e231bc8afc227e43bf9006
Danny <42831259+Dani340@users.noreply.github.com>
2021-03-31 13:26:21 +0300

    Add files via upload

--- 30.3.1.c
+++ 30.3.1.c
@@ -10,7 +10,7 @@ int main() {
     struct node *second = NULL;
     struct node *third = NULL;
     struct node *fourth = NULL;
-    int count, i, arr[10];
+    int count, i;
 
     first = malloc(sizeof(struct node));
     second = malloc(sizeof(struct node));
@@ -23,32 +23,31 @@ int main() {
     fourth->next = NULL;
     head = first;
 
+    struct node *p = head;
+
     printf("Enter the first digit for your number: ");
     scanf("%d", &first->value);
-    arr[0] = first->value;
 
     printf("Enter the second digit for your number: ");
     scanf("%d", &second->value);
-    arr[1] = second->value;
 
     printf("Enter the third digit for your number: ");
     scanf("%d", &third->value);
-    arr[2] = third->value;
 
     printf("Enter the fourth digit for your number: ");
     scanf("%d", &fourth->value);
-    arr[3] = fourth->value;
 
-    for(i = 0; i < 4; i++) {
-        if(arr[i+2] - arr[i+1] == arr[i+1] - arr[i]) {
+    while(p->next != NULL && p->next->next != NULL) {
+        if(p->next->next->value - p->next->value == p->next->value - p->value) {
             printf("True");
             break;
         }
         else {
             count++;
         }
+        p = p->next;
     }
-    if(count == 4) {
+    if(count == 2) {
         printf("False");
     }
 }
