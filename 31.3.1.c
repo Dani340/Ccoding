fbe1876e14e127faf4250ffc2186e759a5bb4a71
Danny <42831259+Dani340@users.noreply.github.com>
2021-03-31 13:48:52 +0300

    Add files via upload

---
+++ 31.3.1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "linkedlist.h"
+
+int main() {
+    int N;
+    struct node *head;
+    struct node *first = NULL;
+    struct node *second = NULL;
+    struct node *third = NULL;
+    struct node *fourth = NULL;
+    int count, max=-1;
+
+    first = malloc(sizeof(struct node));
+    second = malloc(sizeof(struct node));
+    third = malloc(sizeof(struct node));
+    fourth = malloc(sizeof(struct node));
+
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = NULL;
+    head = first;
+
+    struct node *p = head;
+
+    printf("Enter the first digit for your number: ");
+    scanf("%d", &first->value);
+
+    printf("Enter the second digit for your number: ");
+    scanf("%d", &second->value);
+
+    printf("Enter the third digit for your number: ");
+    scanf("%d", &third->value);
+
+    printf("Enter the fourth digit for your number: ");
+    scanf("%d", &fourth->value);
+
+    count = CountAndPrint(&head);
+    printf("\nThe length of the linked list is %d \n", count);
+    printf("\n");
+
+    while(p != NULL) {
+        if(p->value > max) {
+            max = p->value;
+        }
+        p = p->next;
+    }
+    printf("The biggest value of the list is %d", max);
+}
