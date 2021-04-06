f6bab1dd19db9ebd5071c625d9363b2239035b8f
Danny <42831259+Dani340@users.noreply.github.com>
2021-04-06 12:20:58 +0300

    Add files via upload

---
+++ 5.4.2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "doublelinkedlist.h"
+
+int main() {
+    struct node *head;
+    struct node *first = NULL;
+    struct node *second = NULL;
+    struct node *third = NULL;
+    struct node *fourth = NULL;
+    int count, firstvalue;
+
+    first = malloc(sizeof(struct node));
+    second = malloc(sizeof(struct node));
+    third = malloc(sizeof(struct node));
+    fourth = malloc(sizeof(struct node));
+
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = NULL;
+    head = first;
+
+    fourth->prev = third;
+    third->prev = second;
+    second->prev = first;
+    first->prev = NULL;
+
+    printf("Enter the first number: ");
+    scanf("%d", &first->value);
+
+    printf("Enter the second number: ");
+    scanf("%d", &second->value);
+
+    printf("Enter the third number: ");
+    scanf("%d", &third->value);
+
+    printf("Enter the fourth number: ");
+    scanf("%d", &fourth->value);
+
+    printf("Enter a new value for a new node: ");
+    scanf("%d", &firstvalue);
+
+    addfirstNode(&head, firstvalue);
+
+    count = CountAndPrint(&head);
+    printf("\nThe length of the linked list is %d \n", count);
+    printf("\n");
+
+    return 0;
+}
