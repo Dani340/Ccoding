10e72f11caaac9cfee3ef8f61ba8467c9f3a1a26
Danny <42831259+Dani340@users.noreply.github.com>
2021-04-13 18:20:15 +0300

    Update 13.4.1.c

--- 13.4.1.c
+++ 13.4.1.c
@@ -1,47 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "linkedlist.h"
+struct node {
+  int value;
+  struct node *next;
+};
 
-int main() {
-    int N;
-    struct node *head;
-    struct node *first = NULL;
-    struct node *second = NULL;
-    struct node *third = NULL;
-    struct node *fourth = NULL;
-    int count;
+int CircularList(struct node **head) {
 
-    first = malloc(sizeof(struct node));
-    second = malloc(sizeof(struct node));
-    third = malloc(sizeof(struct node));
-    fourth = malloc(sizeof(struct node));
+    struct node *p = *head;
 
-    first->next = second;
-    second->next = third;
-    third->next = fourth;
-    fourth->next = NULL;
-    head = first;
+    while(p != NULL) {
+        if(p->next == *head) {
+            return 0;
+        }
 
-    printf("Enter the first digit for your number: ");
-    scanf("%d", &first->value);
-
-    printf("Enter the second digit for your number: ");
-    scanf("%d", &second->value);
-
-    printf("Enter the third digit for your number: ");
-    scanf("%d", &third->value);
-
-    printf("Enter the fourth digit for your number: ");
-    scanf("%d", &fourth->value);
-
-    printf("\n");
-
-    if(CircularList(&head) == 0) {
-        printf("The list is circular");
-    }
-    else {
-        printf("The list is not circular");
+        p = p->next;
     }
 
-    return 0;
+    return -1;
 }
+
