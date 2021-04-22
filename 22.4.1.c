45058e1df6b330efd96271ef21d75b3a4b343e61
Danny <42831259+Dani340@users.noreply.github.com>
2021-04-22 13:11:04 +0300

    Add files via upload

---
+++ 22.4.1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int v[10];
+    int i, j, n, x, y, X, diff = 100;
+
+    printf("Enter the elements for the vector (enter -1 to finish) \n");
+    scanf("%d", &x);
+    while(x != -1) {
+        v[i] = x;
+        i++;
+        scanf("%d", &x);
+    }
+    n = i;
+
+    printf("Enter X: ");
+    scanf("%d", &X);
+
+    for(i = 0; i < n; i++) {
+        for(j = i+1; j < n; j++) {
+            if(abs(X - (v[i] + v[j])) < diff) {
+                diff = abs(X - (v[i] + v[j]));
+                x = v[i];
+                y = v[j];
+            }
+        }
+    }
+
+    printf("The two numbers are: %d %d", x, y);
+
+    return 0;
+}
