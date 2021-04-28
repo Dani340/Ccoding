573a24e533bfbf2756378cd4e1b9ec935d95d08f
Danny <42831259+Dani340@users.noreply.github.com>
2021-04-28 12:22:57 +0300

    Add files via upload

--- stack.h
+++ stack.h
@@ -24,11 +24,25 @@ bool isFull(struct stack *p) {
 }
 
 void printAll(struct stack *p) {
+    int i, count = 0;
 
     printf("All of the elements are: ");
-    for (int i = 0; i <= p->top; i++) {
+    for (i = 0; i <= p->top; i++) {
         printf("%d ", p->items[i]);
+        count++;
     }
     printf("\n");
+    printf("The stack has %d elements\n", count);
 
+}
+
+void push(struct stack *p, int value) {
+    if (isFull(p) == true) {
+        printf("The stack is full!");
+    }
+    else {
+        p->top++;
+        p->items[p->top] = value;
+        printf("The value %d was added to the stack\n", value);
+    }
 }
