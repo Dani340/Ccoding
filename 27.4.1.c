e00d00199edc872980ff56ca1aee57fefdec26e2
Danny <42831259+Dani340@users.noreply.github.com>
2021-04-29 10:53:56 +0300

    Add files via upload

--- 27.4.1.c
+++ 27.4.1.c
@@ -1,20 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-struct Node {
-    int value;
-    struct Node *next;
-};
-
-bool isEmpty(struct Node **top) {
-    if (top == NULL) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
+#include "stackLL.h"
 
 int main() {
     struct Node *top;
