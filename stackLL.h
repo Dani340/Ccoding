807655beb7eb0dae82b4cc7100ea89fedab5c03e
Danny <42831259+Dani340@users.noreply.github.com>
2021-05-04 13:03:24 +0300

    Add files via upload

--- stackLL.h
+++ stackLL.h
@@ -3,7 +3,21 @@ struct Node {
     struct Node *next;
 };
 
+struct node {
+    char value;
+    struct node *next;
+};
+
 bool isEmpty(struct Node **top) {
+    if (top == NULL) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool isEmptychar(struct node **top) {
     if (top == NULL) {
         return true;
     }
@@ -24,6 +38,16 @@ void push(struct Node **top, int value) {
     printf("The value %d was added at the top of the stack\n", value);
 }
 
+void pushchar(struct node **top, int value) {
+
+    struct node *p;
+    p = malloc(sizeof(struct node));
+
+    p->value = value;
+    p->next = (*top);
+    (*top) = p;
+}
+
 void pop(struct Node **top) {
 
     struct Node *p;
@@ -38,6 +62,22 @@ void pop(struct Node **top) {
         p->next = NULL;
 
         printf("The value %d was deleted from the stack\n", p->value);
+        free(p);
+    }
+}
+
+void popchar(struct node **top) {
+
+    struct node *p;
+    p = malloc(sizeof(struct node));
+
+    if (isEmptychar(top) == true) {
+        printf("The list is empty");
+    }
+    else {
+        p = (*top);
+        (*top) = (*top)->next;
+        p->next = NULL;
         free(p);
     }
 }
