891408e279f18811d03523983e50a8468e25f059
Danny <42831259+Dani340@users.noreply.github.com>
2021-05-05 12:00:12 +0300

    Add files via upload

---
+++ 5.5.1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <regex.h>
+
+int main() {
+
+    char S[30];
+    int result;
+
+    regex_t regex;
+
+    printf("Enter a string to match: ");
+    scanf("%s", S);
+
+    result = regcomp(&regex, "[a-zA-Z\\s]+\\.$", 0);
+    result = regexec(&regex, S, 0, NULL, 0);
+
+    if(result == 0) {
+        printf("True");
+    }
+
+    else {
+        printf("False");
+    }
+
+    return 0;
+}
