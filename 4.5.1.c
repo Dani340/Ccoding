aa71a4539f74006bf8e7b3acc73ebf82f25150f9
Danny <42831259+Dani340@users.noreply.github.com>
2021-05-05 09:13:57 +0300

    Add files via upload

--- 4.5.1.c
+++ 4.5.1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 #include "stackLL.h"
 
@@ -7,7 +8,7 @@ int main() {
     struct node *top;
     struct node *p;
     struct node *first = NULL;
-    char W[10];
+    char W[10], res;
     int i = 1;
 
     first = malloc(sizeof(struct node));
@@ -18,15 +19,14 @@ int main() {
 
     first->value = W[0];
 
-    while(W[i] != '\0') {
+    for (i = 1; i < strlen(W); i++) {
         pushchar(&top, W[i]);
-        i++;
     }
 
     while(p != NULL) {
         p = top;
-        printf("%c", p->value);
-        popchar(&top);
+        res = popchar(&top);
+        printf("%c", res);
     }
 
     return 0;
