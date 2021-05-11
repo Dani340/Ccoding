67088074ddf12e6d4a298da599989b9ef5769e10
Danny <42831259+Dani340@users.noreply.github.com>
2021-05-11 14:19:35 +0300

    Add files via upload

--- queue.h
+++ queue.h
@@ -1,16 +1,49 @@
 struct QNode {
     int value;
     struct QNode* next;
-    struct QNode* front;
-    struct QNode* rear;
 };
 
-bool isEmpty(struct QNode *q) {
+struct Queue {
+    struct QNode *front, *rear;
+};
+
+bool isEmpty(struct Queue *q) {
 
     if(q->front == NULL && q->rear == NULL) {
         return true;
     }
     else {
         return false;
+    }
+}
+
+void enQueue(struct Queue* q, int value) {
+    struct QNode *p;
+
+    p = malloc(sizeof(struct QNode));
+    p->value = value;
+
+    if (q->rear == NULL) {
+        q->front = q->rear = p;
+        printf("The value %d was added", value);
+    }
+    else {
+        p->next = q->rear;
+        q->rear = p;
+        printf("The value %d was added", value);
+    }
+}
+
+void print(struct QNode* p) {
+
+    printf("The list is: ");
+    if(p == NULL) {
+        printf("Queue is empty");
+    }
+    else {
+        while(p != NULL) {
+            printf("%d ", p->value);
+            p = p->next;
+        }
     }
 }
