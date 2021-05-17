ea376edbc14393060e34bf0e94c2a51bdb263220
Danny <42831259+Dani340@users.noreply.github.com>
2021-05-17 18:35:27 +0300

    Add files via upload

---
+++ 17.5.2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* recursive(char* word, char* reversed, int position) {
+    if(position >= 0) {
+        recursive(word, strcat(reversed, &word[position]), position-1);
+    }
+    return reversed;
+}
+
+int main(int argc, char *argv[])  {
+    char word[50];
+    char* reversed;
+    reversed = malloc (strlen(word) * sizeof (char));
+    int i = 0;
+
+    if(argc == 2) {
+        strcpy(word, argv[1]);
+        recursive(word, reversed, strlen(word) - 1);
+        printf("%s", reversed);
+    }
+    else if(argc > 2) {
+      printf("Too many arguments entered\n");
+    }
+    else {
+        printf("You did not enter the string\n");
+    }
+}
