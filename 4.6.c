a1e798a03a3adfb457d61389bb7feb5898aa2fef
Danny <42831259+Dani340@users.noreply.github.com>
2021-06-04 12:31:49 +0300

    Add files via upload

---
+++ 4.6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int i,j,N;
+
+    printf("Enter the number: ");
+    scanf("%d",&N);
+
+    for(i = 0; i < N; i++) {
+        for(j = 0; j < N-i-1; j++) {
+            printf(" ");
+        }
+        for(j = 0; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
