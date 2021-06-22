eb0a7e19fb81736b0ef09a25ea80c89fe8178ec5
Danny <42831259+Dani340@users.noreply.github.com>
2021-06-22 15:10:17 +0300

    Add files via upload

--- 21.6.c
+++ 21.6.c
@@ -2,8 +2,8 @@
 #include <math.h>
 int matrix[256][256];
 
-int impartire(int iMin, int iMax, int jMin, int jMax, int array[]) {
-    int i, j, array1[64], array2[64], array3[64], array4[64], arraylength;
+void impartire(int iMin, int iMax, int jMin, int jMax, int array[]) {
+    int i, j, array1[64], array2[64], array3[64], array4[64], arraylength, k = 0;
 
     if(iMax - iMin == 2 && jMax - jMin == 2) {
         for(i = iMin; i < iMax; i++) {
@@ -11,31 +11,38 @@ int impartire(int iMin, int iMax, int jMin, int jMax, int array[]) {
                 matrix[i][j] = array[i];
             }
         }
+        return;
     }
 
-    arraylength = sizeof(array) / sizeof(array[0]);
-    for(i = 0; i < arraylength / 4; i++) {
+    arraylength = sizeof(array) / sizeof(array[0]) / 4;
+    for(i = 0; i < arraylength; i++) {
         array1[i] = i;
     }
-    for(i = arraylength / 4; i < 2 * arraylength / 4; i++) {
-        array2[i] = i;
+    for(i = arraylength; i < 2 * arraylength; i++) {
+        array2[k] = i;
+        k++;
     }
-    for(i = 2 * arraylength / 4; i < 3 * arraylength / 4; i++) {
-        array3[i] = i;
+    k = 0;
+    for(i = 2 * arraylength; i < 3 * arraylength; i++) {
+        array3[k] = i;
+        k++;
     }
-    for(i = 3 * arraylength / 4; i < arraylength / 4; i++) {
-        array4[i] = i;
+    k = 0;
+    for(i = 3 * arraylength; i < 4 * arraylength; i++) {
+        array4[k] = i;
+        k++;
     }
     impartire(iMin, iMax/2 - 1, jMin, jMax/2 - 1, array1);
-    impartire(iMax/2 , iMax, jMin, jMax/2 - 1, array2);
-    impartire(iMin, iMax/2 - 1, jMax/2, jMax, array3);
-    impartire(iMax/2 , iMax, jMax/2 , jMax, array4);
+    impartire(iMin, iMax/2 - 1, jMax/2, jMax - 1, array2);
+    impartire(iMax/2 , iMax - 1, jMin, jMax/2 - 1, array3);
+    impartire(iMax/2 , iMax - 1, jMax/2 , jMax - 1, array4);
  }
 
 int main() {
     int i, j, N, size, x, y;
+    int array[256];
 
-    printf("Enter the number: ");
+    printf("Enter the number of cols and rows of the matrix: ");
     scanf("%d",&N);
 
     printf("Enter the coord of the element you want to be printed: ");
@@ -43,6 +50,11 @@ int main() {
     scanf("%d",&y);
 
     size = pow(2, N);
+
+    for(i = 0; i < size*size; i++) {
+        array[i] = i;
+    }
+    impartire(0, size, 0, size, array);
 
     for(i = 0; i < size; i++) {
         for(j = 0; j < size; j++) {
