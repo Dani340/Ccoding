f2e03ed9275a334178a6003f2e4ad16a9ce29923
Danny <42831259+Dani340@users.noreply.github.com>
2021-07-06 06:07:42 -0700

    Add files via upload

---
+++ 6.7.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "graph.h"
+
+int main() {
+    int i, x,  N, M, src[20], dest[20], array[40], lowest = 100;
+
+    printf("Enter the number of nodes for the graph: ");
+    scanf("%d",&N);
+
+    printf("Enter the number of edges: ");
+    scanf("%d",&M);
+
+    x = 0;
+    for(i = 0; i < M; i++) {
+        printf("Enter the elements of the %d edge\n", i+1);
+        scanf("%d", &src[i]);
+        scanf("%d", &dest[i]);
+
+        array[x] = src[i];
+        array[x+1] = dest[i];
+
+        if(array[x] < lowest) {
+            lowest = array[x];
+        }
+        else if(array[x+1] < lowest) {
+            lowest = array[x+1];
+        }
+
+        x = x+2;
+    }
+
+    struct Graph* graph = createGraph(array, 2*M);
+
+    for(i = 0; i < M; i++) {
+        addEdge(graph, src[i], dest[i]);
+    }
+
+    DFS(graph, lowest);
+
+    return 0;
+}
