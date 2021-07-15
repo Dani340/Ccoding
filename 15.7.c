25e0b419b2afeb6ad0722bc906d1b3c1679c6778
Danny <42831259+Dani340@users.noreply.github.com>
2021-07-15 15:27:31 +0300

    Add files via upload

---
+++ 15.7.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "graph.h"
+
+int main() {
+    int i, x, N, M, src[20], dest[20], array[40];
+
+    printf("Enter the number of nodes for the graph: ");
+    scanf("%d",&N);
+
+    printf("Enter the number of edges: ");
+    scanf("%d",&M);
+
+    x = 0;
+    for(i = 0; i < M; i++) {
+        printf("Enter the elements of the %d edge\n", i+1);
+        scanf("%d", &src[i]);
+        scanf("%d", &dest[i]);
+
+        array[x] = src[i];
+        array[x+1] = dest[i];
+
+        x = x+2;
+    }
+
+    struct Graph* graph = createGraph(array, 2*M);
+
+    for(i = 0; i < M; i++) {
+        addEdge(graph, src[i], dest[i]);
+    }
+
+    CityBuy(graph);
+
+    return 0;
+}
