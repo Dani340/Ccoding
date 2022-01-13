388da96e12485269841dff21a4fc9efe3e0873cf
Danny <42831259+Dani340@users.noreply.github.com>
2022-01-13 16:42:44 +0200

    Add files via upload

--- 13.1.2.c
+++ 13.1.2.c
@@ -1,23 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 int main() {
-    int firstn, num, i = 1, originalnum, result;
+    int i, n, m, c, sum = 0;
+    int nsir[75], msir[150];
+    char *mstr, *nstr, *sirstr;
+    char S[75];
+    const char s[2] = " ";
+    FILE *fptr;
 
-    printf("Enter your number:");
-    scanf("%d", &num);
-    firstn = num;
-
-    num = num / 100;
-    originalnum = num;
-
-    while (num > 99) {
-        num = num / 10;
-        i = i * 10;
+    if ((fptr = fopen("cautare1.in.txt", "r")) == NULL) {
+        printf("Error! opening file");
+        exit(1);
     }
 
-    i = i * num;
-    originalnum = originalnum - i;
+    fgets(S, 10, fptr);
+    nstr = strtok(S, s);
+    n = atoi(nstr);
 
-    printf("The result is %d", originalnum);
+    mstr = strtok(NULL, s);
+    m = atoi(mstr);
+
+    fgets(S, 75, fptr);
+    sirstr = strtok(S, s);
+    nsir[0] = atoi(sirstr);
+
+    for(i = 1; i < n; i++) {
+        sirstr = strtok(NULL, s);
+        nsir[i] = atoi(sirstr);
+    }
+
+    fgets(S, 75, fptr);
+    sirstr = strtok(S, s);
+    msir[0] = atoi(sirstr);
+
+    for(c = 0; c < n; c++) {
+        if(nsir[c] == msir[i]) {
+            sum += c+1;
+            break;
+        }
+    }
+    printf("%d\n", sum);
+
+    for(i = 1; i < m; i++) {
+        sum = 0;
+        sirstr = strtok(NULL, s);
+        msir[i] = atoi(sirstr);
+
+        for(c = 0; c < n; c++) {
+            if(nsir[c] == msir[i]) {
+                sum += c+1;
+                break;
+            }
+        }
+        printf("%d\n", sum);
+    }
 
 }
