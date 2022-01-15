c2980f09010f119ca9c033f91cdf4e433a7869e0
Danny <42831259+Dani340@users.noreply.github.com>
2022-01-15 14:08:13 +0200

    Add files via upload

--- 14.1.2.c
+++ 14.1.2.c
@@ -1,23 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 int main() {
-    int firstn, num, i = 1, last2, result;
+    int i, u, n, x, j, t, sum = 0;
+    char *nstr, *tstr, *xstr;
+    char S[200];
+    const char s[2] = " ";
+    FILE *fptr;
 
-    printf("Enter your number:");
-    scanf("%d", &num);
-    firstn = num;
-
-    last2 = num % 100;
-    num = num / 10;
-
-    while (num > 99) {
-        num = num / 10;
-        i = i * 10;
+    if ((fptr = fopen("marcel.in.txt", "r")) == NULL) {
+        printf("Error! opening file");
+        exit(1);
     }
 
-    i = i * num;
-    i = last2 + i;
+    fgets(S, 10, fptr);
+    nstr = strtok(S, s);
+    n = atoi(nstr);
 
-    printf("The result is %d", i);
+    i = 0;
+    j = 0;
+    for(u = 0; u < n; u++) {
+        fgets(S, 10, fptr);
+        tstr = strtok(S, s);
+        t = atoi(nstr);
 
+        xstr = strtok(NULL, s);
+        x = atoi(xstr);
+
+        if(t == 0) {
+            j += x;
+        }
+        else {
+            i += x;
+        }
+
+        if((i + j) % 2 == 0) {
+            sum += i+j+2;
+        }
+        else {
+            sum -= i+j+2;
+        }
+    }
+
+    printf("%d", sum);
+
+    return 0;
 }
