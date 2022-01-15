5ebdcd99b12ad98173c83b51e2d008fde9a29f1f
Danny <42831259+Dani340@users.noreply.github.com>
2022-01-15 14:08:01 +0200

    Add files via upload

--- 14.1.1.c
+++ 14.1.1.c
@@ -1,37 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 int main() {
-    int N, c;
-    float x, y, sum=0;
+    int i, n, x, j, u, auxn1, auxn2;
+    char sir[100][20];
+    char *nstr, *sirstr;
+    char S[200];
+    const char s[2] = " ";
     FILE *fptr;
 
-    if ((fptr = fopen("media.in.txt", "r")) == NULL) {
+    if ((fptr = fopen("codificare.in.txt", "r")) == NULL) {
         printf("Error! opening file");
         exit(1);
     }
 
-    fscanf(fptr, "%d\n", &N);
-    for (c = 0; c < N; c++)
-    {
-        fscanf(fptr,"%f\n", &x);
-        sum += x;
+    fgets(S, 10, fptr);
+    nstr = strtok(S, s);
+    n = atoi(nstr);
+
+    fgets(S, 200, fptr);
+    sirstr = strtok(S, s);
+
+    x = 0;
+    auxn1 = 0;
+    auxn2 = n;
+    for(i = 0; i < strlen(sirstr) / n; i++) {
+        j = 0;
+        if(x % 2 == 0) {
+            for(u = auxn1; u < auxn2; u++) {
+                sir[i][j] = sirstr[u];
+                j++;
+            }
+        }
+        else {
+            for(u = auxn2-1; u >= auxn1; u--) {
+                sir[i][j] = sirstr[u];
+                j++;
+            }
+        }
+        auxn1 += n;
+        auxn2 += n;
+        x++;
     }
 
-    sum = sum / 4.0 * 3.0;
-
-    fscanf(fptr, "%f\n", &y);
-    sum = (sum + y) / 4.0;
-
-
-	fclose(fptr);
-
-	FILE * fout;
-     if ((fout = fopen("media.out.txt", "w")) == NULL) {
-        printf("Error! opening file");
-        exit(1);
+    for(j = 0; j < n; j++) {
+        for(i = 0; i < strlen(sirstr) / n; i++) {
+            printf("%c", sir[i][j]);
+        }
     }
 
-    fprintf(fout,"%f\n",sum);
-    fclose(fout);
-
+    return 0;
 }
