c08f1884167d4cf3f8b50a7563549283a6469931
Danny <42831259+Dani340@users.noreply.github.com>
2022-01-21 14:37:37 +0200

    Add files via upload

--- 21.1.2.c
+++ 21.1.2.c
@@ -1,26 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-  int main() {
-        int x, y, restx=0, resty = 0, count=0;
+int main() {
+    int i, j, v[50], t[50], n, x, sum, count, poz, max = -1;
+    char *nstr, *vstr, *tstr;
+    char S[75];
+    const char s[2] = " ";
+    FILE *fptr;
 
-        printf("Enter your number:");
-        scanf("%d", &x);
+    if ((fptr = fopen("asasin.in.txt", "r")) == NULL) {
+        printf("Error! opening file");
+        exit(1);
+    }
 
-        printf("Enter your number:");
-        scanf("%d", &y);
+    fgets(S, 10, fptr);
+    nstr = strtok(S, s);
+    n = atoi(nstr);
 
-        while (x != 0) {
-                restx = x % 10;
-                resty = y % 10;
+    for(i = 0; i < n; i++) {
+        fgets(S, 10, fptr);
+        vstr = strtok(S, s);
+        v[i] = atoi(vstr);
 
-                if(restx + resty >= 10) {
-                    count++;
+        tstr = strtok(NULL, s);
+        t[i] = atoi(tstr);
+    }
+
+    for(i = 0; i < n; i++) {
+        for(j = n-1; j > i; j--) {
+            sum = 0;
+            for(x = i; x < j; x++) {
+                if(t[x] == -1) {
+                    sum -= v[x];
                 }
-
-                x = x / 10;
-                y = y / 10;
+                else if(t[x] == 1) {
+                    sum += v[x];
+                }
+            }
+            if(sum > max) {
+                max = sum;
+                count = j-i;
+                poz = i;
+            }
+            else if(sum == max) {
+                if(poz > i) {
+                    poz = i;
+                    count = i-j;
+                }
+                else if(poz == i) {
+                    if(count > j-i) {
+                        count = j-i;
+                    }
+                }
+            }
         }
+    }
 
-        printf("There are %d pairs bigger than 10", count);
-        return 0;
-  }
+    printf("%d %d %d", max, poz+1, count);
+
+    return 0;
+}
+
+
