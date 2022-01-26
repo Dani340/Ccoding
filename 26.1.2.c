7d45febd11521bc3ffc8cd2e0afbbe115a4017d7
Danny <42831259+Dani340@users.noreply.github.com>
2022-01-26 13:44:17 +0200

    Add files via upload

--- 26.1.2.c
+++ 26.1.2.c
@@ -1,52 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-  int main() {
-        int num, biggestdig=0, biggestno = 0, originalno, originalno2, lowestdig=0, mediumdig, rest = 0;
-
-        printf("Enter your number:");
-        scanf("%d", &num);
-
-        originalno = num;
-        originalno2 = num;
-
-        while (num > 0) {
-            rest = num % 10;
-
-            if (rest > biggestdig) {
-                biggestdig = rest;
-            }
-
-            num = num / 10;
+int main() {
+    int i, j, sir[100], ang[100][100], x[100] = {0}, n;
+    char *nstr, *sirstr;
+    char S[75];
+    const char s[2] = " ";
+    for(i = 0; i < 100; i++) {
+        for(j = 0; j < 100; j++) {
+            ang[i][j] = -1;
         }
+    }
 
-        biggestno += biggestdig * 100;
+    FILE *fptr;
 
-        lowestdig = biggestdig;
+    if ((fptr = fopen("ierarhie.in.txt", "r")) == NULL) {
+        printf("Error! opening file");
+        exit(1);
+    }
 
-        while (originalno > 0) {
-            rest = originalno % 10;
+    fgets(S, 10, fptr);
+    nstr = strtok(S, s);
+    n = atoi(nstr);
 
-            if (rest < lowestdig) {
-                lowestdig = rest;
+    fgets(S, 100, fptr);
+    sirstr = strtok(S, s);
+    sir[0] = atoi(sirstr);
+    ang[sir[0]][x[sir[0]]] = 1;
+    x[sir[0]]++;
+
+    for(i = 1; i < n; i++) {
+        sirstr = strtok(NULL, s);
+        sir[i] = atoi(sirstr);
+        ang[sir[i]][x[sir[i]]] = i+1;
+        x[sir[i]]++;
+    }
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < x[i]; j++) {
+            if(ang[i][j] != -1 && j == 0) {
+                if(i != 0) {
+                    printf("%d %d ", i, ang[i][j]);
+                }
+                else {
+                    printf("%d", ang[i][j]);
+                }
             }
-
-            originalno = originalno / 10;
-        }
-
-
-        while (originalno2 > 0) {
-            rest = originalno2 % 10;
-
-            if (rest < biggestdig && rest > lowestdig) {
-                mediumdig = rest;
+            else if(ang[i][j] != -1) {
+                printf("%d ", ang[i][j]);
             }
-
-            originalno2 = originalno2 / 10;
         }
+        if(ang[i][x[i]-1] != -1) {
+            printf("\n");
+        }
+    }
 
-        biggestno += 10 * mediumdig + lowestdig;;
-
-        printf("%d", biggestno);
-
-        return 0;
-  }
+    return 0;
+}
