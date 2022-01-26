085a2013ae57634f255c2e6088296673c25decab
Danny <42831259+Dani340@users.noreply.github.com>
2022-01-26 13:44:05 +0200

    Add files via upload

--- 26.1.1.c
+++ 26.1.1.c
@@ -1,37 +1,40 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-int main()
-{
-    char str[80];
-    char substr[10];
-    int i, j, result;
+int main() {
+    int i, nr = 1, sir[50], count = -1, n;
+    char *nstr, *sirstr;
+    char S[75];
+    const char s[2] = " ";
+    FILE *fptr;
 
-    printf("Enter a string:");
-    gets(str);
+    if ((fptr = fopen("carti1.in.txt", "r")) == NULL) {
+        printf("Error! opening file");
+        exit(1);
+    }
 
-    printf("Enter a substring:");
-    gets(substr);
+    fgets(S, 10, fptr);
+    nstr = strtok(S, s);
+    n = atoi(nstr);
 
-    for (i = 0; i <= strlen(str) - strlen(substr); i++) {
-        for (j = i; j < i + strlen(substr); j++) {
-            result = 1;
-            if (str[j] != substr[j - i]) {
-                result = 0;
-                break;
+    for(i = 0; i < n; i++) {
+        fgets(S, 10, fptr);
+        sirstr = strtok(S, s);
+        sir[i] = atoi(sirstr);
+    }
+
+    while(nr != n+1) {
+        count++;
+        for(i = 0; i < n; i++) {
+            if(sir[i] == nr) {
+                nr++;
             }
         }
-        if (result == 1) {
-            break;
-        }
     }
 
-    if (result == 1) {
-        printf("True");
-    }
-    else {
-        printf("False");
-    }
+    printf("%d", count);
 
     return 0;
 }
