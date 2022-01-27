4257d907b0254b8f86ecbab1f53c3615a4ffac9a
Danny <42831259+Dani340@users.noreply.github.com>
2022-01-27 14:16:06 +0200

    Add files via upload

--- 27.1.1.c
+++ 27.1.1.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-int main()
-{
-   char str[50];
+int main() {
+    int n, count = 0, p = 1;
+    char *nstr;
+    char S[75];
+    const char s[2] = " ";
+    FILE *fptr;
 
-   printf("Enter a string: ");
-   gets(str);
+    if ((fptr = fopen("cluburi.in.txt", "r")) == NULL) {
+        printf("Error! opening file");
+        exit(1);
+    }
 
-   strrev(str);
+    fgets(S, 10, fptr);
+    nstr = strtok(S, s);
+    n = atoi(nstr);
 
-   printf("The reversed version of the string: %s \n", str);
+    while(n != 0) {
+        if(n % 2 == 1) {
+            printf("%d ", p);
+            count++;
+        }
+        n = n / 2;
+        p *= 2;
+    }
 
-   return 0;
+    printf("\n%d", count);
+
+    return 0;
 }
