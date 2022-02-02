9fe8de71cc7bfa0cdb00fc90736373801be33c96
Danny <42831259+Dani340@users.noreply.github.com>
2022-02-02 22:02:31 +0200

    Add files via upload

--- 1.2.1.c
+++ 1.2.1.c
@@ -1,31 +1,60 @@
 #include <stdio.h>
-int main()
-{
-    char string1[100];
-    char X;
-    char Y;
-    int  i;
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
-    printf("Enter a word or a sentence: ");
-    gets(string1);
+int main() {
+    long long int i, j, c, p, num;
+    bool bool1;
+    int sir;
+    char *sirstr;
+    char S[100];
+    const char s[2] = " ";
+    FILE *fptr;
 
-    printf("Enter a letter: ");
-    scanf(" %c", &X);
-
-    printf("Enter another letter: ");
-    scanf(" %c", &Y);
-
-
-    for(i = strlen(string1); i > 0; i--)
-    {
-        if(string1[i] == X) {
-            string1[i] = Y;
-            break;
-        }
+    if ((fptr = fopen("factori.in.txt", "r")) == NULL) {
+        printf("Error! opening file");
+        exit(1);
     }
 
-    printf("%s", string1);
 
+    fgets(S, 5, fptr);
+    sirstr = strtok(S, s);
+    sir = atoi(sirstr);
+
+    while(sir != 0) {
+        c = 2;
+        num = 1;
+        for(i = 2; i <= sir; i++) {
+            num *= i;
+        }
+        while(num != 1) {
+            p = 0;
+            bool1 = true;
+            for(j = 2; j <= c/2; j++) {
+                if(c % j == 0) {
+                    bool1 = false;
+                    break;
+                }
+            }
+            if(bool1 == true) {
+                while(num % c == 0 && num != 1) {
+                    p++;
+                    num = num / c;
+                }
+                if(p != 0) {
+                    printf("%d ", p);
+                }
+            }
+            c++;
+        }
+
+        fgets(S, 5, fptr);
+        sirstr = strtok(S, s);
+        sir = atoi(sirstr);
+        printf("\n");
+    }
 
     return 0;
 }
