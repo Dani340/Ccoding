a6114b2e0378494f52d6d48b08198110a1f6b191
Danny <42831259+Dani340@users.noreply.github.com>
2022-02-05 15:44:55 +0200

    Add files via upload

--- 4.2.2.c
+++ 4.2.2.c
@@ -1,33 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
-int main()
-{
-    int A[100];
-    int  N, i, X, count=0;
+int main() {
+    int i, x = 0, r = 0, t = 0;
+    int sir[100], sir2[100], n;
+    bool bool1 = false;
+    char *nstr, *sirstr;
+    char S[75];
+    const char s[2] = " ";
+    FILE *fptr;
 
-    printf("Enter the number of elements you want for the list: ");
-    scanf(" %d", &N);
+    if ((fptr = fopen("ruleta1.in.txt", "r")) == NULL) {
+        printf("Error! opening file");
+        exit(1);
+    }
 
-    printf("Enter X: ");
-    scanf(" %d", &X);
+    fgets(S, 10, fptr);
+    nstr = strtok(S, s);
+    n = atoi(nstr);
 
-    printf("Enter %d numbers \n", N);
+    fgets(S, 100, fptr);
+    sirstr = strtok(S, s);
+    sir[0] = atoi(sirstr);
 
-    for(i = 0; i < N; i++)
-    {
-        scanf("%d", &A[i]);
-        if(X == A[i]) {
-            count++;
+    for(i = 1; i < n; i++) {
+        sirstr = strtok(NULL, s);
+        sir[i] = atoi(sirstr);
+    }
+
+    while(bool1 == false) {
+        for(i = 0; i < n; i++) {
+            sir2[i] = sir[i];
+        }
+
+        for(i = 0; i < n; i++) {
+            if(i != n-1) {
+                if(sir[i] <= sir[i+1]) {
+                    sir[i+1] -= sir[i];
+                }
+            }
+            else {
+                if(sir[i] <= sir[0]) {
+                    sir[0] -= sir[i];
+                }
+            }
+        }
+
+        bool1 = true;
+        for(i = 0; i < n; i++) {
+            if(sir[i] != sir2[i]) {
+                bool1 = false;
+            }
+        }
+        r++;
+    }
+
+    for(i = 0; i < n; i++) {
+        if(sir[i] != 0) {
+            t++;
         }
     }
 
-    if(count == 0) {
-        printf("False");
-    }
-
-    else {
-        printf("True");
-    }
+    printf("%d %d",r,t);
 
     return 0;
 }
