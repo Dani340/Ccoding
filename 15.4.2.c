d86c2215d25bce0572ca046c246160d6ac660ea8
Danny <42831259+Dani340@users.noreply.github.com>
2022-04-16 13:25:49 +0300

    Add files via upload

--- 15.4.2.c
+++ 15.4.2.c
@@ -1,40 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 int main() {
-    int i, j, N, rest, arr[20];
+    int i, j, p, count = 0, sir[100], n;
+    int p1[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    int p2[] = {2, 3, 4, 5, 6, 7, 8, 9};
+    int p3[] = {10, 11, 12, 13, 14, 15};
+    bool bool1 = false;
+    char *pstr, *nstr, *sirstr;
+    char S[100];
+    const char s[2] = "#";
+    FILE *fptr;
 
-    printf("Enter the number of elements for the array: ");
-    scanf("%d", &N);
-
-    printf("Enter the elements\n");
-    for(i = 0; i < N; i++) {
-        scanf("%d", &arr[i]);
+    if ((fptr = fopen("cod.in.txt", "r")) == NULL) {
+        printf("Error! opening file");
+        exit(1);
     }
 
-    for(i = 0; i < N; i++) {
-        if(arr[i] % 2 == 1) {
-            for(j = i+1; j < N; j++) {
-                if(arr[j] % 2 == 0) {
-                    rest = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = rest;
+    fgets(S, 5, fptr);
+    pstr = strtok(S, " ");
+    p = atoi(pstr);
+
+    fgets(S, 5, fptr);
+    nstr = strtok(S, " ");
+    n = atoi(nstr);
+
+    fgets(S, 100, fptr);
+    sirstr = strtok(S, s);
+    sir[0] = atoi(sirstr);
+
+    for(i = 1; i < n; i++) {
+        sirstr = strtok(NULL, s);
+        sir[i] = atoi(sirstr);
+    }
+
+    if(p == 1) {
+        for(i = 0; i < n; i++) {
+            bool1 = false;
+            for(j = 0; j < 14; j++) {
+                if(sir[i] % p1[j] == 0) {
+                    bool1 = true;
                     break;
                 }
             }
+            if(bool1 == false) {
+                count++;
+            }
         }
+        printf("%d", count);
     }
-    printf("The ordered list is: ");
-    for(i = 0; i < N; i++) {
-        if(arr[i] % 2 == 1) {
-            for(j=i+1; j < N; j++) {
-                if(arr[j] % 2 == 1 && arr[j] < arr[i]) {
-                    rest = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = rest;
+    else if(p == 2) {
+        for(i = 0; i < n; i++) {
+            for(j = 0; j < 8; j++) {
+                if(sir[i] % p2[j] == 0) {
+                    count++;
                 }
             }
         }
-        printf("%d ", arr[i]);
+        printf("%d", count);
     }
+    else {
+        for(i = 0; i < n; i++) {
+            for(j = 0; j < 6; j++) {
+                if(sir[i] % p3[j] == 0) {
+                    count++;
+                }
+            }
+        }
+        printf("%d", count);
+    }
+
+    return 0;
 }
