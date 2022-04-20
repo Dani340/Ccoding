e4f7091f0d3c4844b5491ee020c89000ffaea28a
Danny <42831259+Dani340@users.noreply.github.com>
2022-04-20 17:20:37 +0300

    Add files via upload

--- 20.4.1.c
+++ 20.4.1.c
@@ -1,30 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 int main() {
-    char res[10];
-    int dec, j, rest, i=0;
+    int i, j, aux, x = 0, sirl[100], sir[100], sirnou[100], n;
+    for(i = 0; i < 100; i++) {
+        sirl[i] = 0;
+    }
+    bool bool1;
+    char *nstr, *sirstr;
+    char S[100];
+    const char s[2] = " ";
+    FILE *fptr;
 
-    printf("Enter the decimal number: ");
-    scanf("%d", &dec);
-
-    while (dec != 0) {
-        rest = 0;
-        rest = dec % 16;
-
-        if (rest < 10) {
-            res[i] = rest + 48; // pt dec-char //
-            i++;
-        }
-        else {
-            res[i] = rest + 55;
-            i++;
-        }
-
-        dec = dec / 16;
+    if ((fptr = fopen("cod5.in.txt", "r")) == NULL) {
+        printf("Error! opening file");
+        exit(1);
     }
 
-    printf("The number converted to hexadecimal is:");
-    for(j = i; j >= 0; j--) {
-        printf("%c", res[j]);
+    fgets(S, 5, fptr);
+    nstr = strtok(S, s);
+    n = atoi(nstr);
+
+    fgets(S, 100, fptr);
+    sirstr = strtok(S, s);
+    sir[0] = atoi(sirstr);
+
+    for(i = 1; i < n; i++) {
+        sirstr = strtok(NULL, s);
+        sir[i] = atoi(sirstr);
     }
+
+    for(i = 0; i < n; i++) {
+        if(sirl[i] == 0) {
+            bool1 = false;
+            for(j = i+1; j < n; j++) {
+                if(sir[i] == sir[j] && sirl[j] == 0) {
+                    sirl[j] = 1;
+                    bool1 = true;
+                    break;
+                }
+            }
+            if(bool1 == false) {
+                sirnou[x] = sir[i];
+                x++;
+            }
+        }
+    }
+
+    for(i = 0; i < x; i++) {
+        for(j = i+1; j < x; j++) {
+            if(sirnou[i] > sirnou[j]) {
+                aux = sirnou[i];
+                sirnou[i] = sirnou[j];
+                sirnou[j] = aux;
+            }
+        }
+    }
+
+    for(i = 0; i < x; i++) {
+        printf("%d", sirnou[i]);
+    }
+
+    return 0;
 }
+
